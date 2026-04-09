ad4697d6b62c0aa8ddd6f9934dfba1076f0e0bf4
kdobrovolsky <kdobrovolsky02@gmail.com>
2026-04-09 21:46:40 +0300

    fix vercel

--- support-admin/src/lib/supabase/config.ts
+++ support-admin/src/lib/supabase/config.ts
@@ -1,5 +1,5 @@
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseAnonKey = process.env.NEXT_SB_SECRET;
 
 export function getSupabaseConfig() {
   if (!supabaseUrl || !supabaseAnonKey) {
